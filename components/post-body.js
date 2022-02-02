447b58364692234a4e9e95171e4e5ae7f305a751
Hasan Akalp <hasan.akalp@gmail.com>
2022-02-02 19:35:10 +0300

    update tag and class for typography

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,8 @@
-import markdownStyles from './markdown-styles.module.css'
-
 export default function PostBody({ content }) {
   return (
     <div className="max-w-5xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
+      <article
+        className="prose lg:prose-xl dark:prose-invert"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
