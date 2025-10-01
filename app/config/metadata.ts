3b2f3d68851ad00b6242b047fc8738dcc0bbb5a6
akalp <hasan.akalp@gmail.com>
2025-10-02 00:18:59 +0300

    changes for static

--- app/config/metadata.ts
+++ app/config/metadata.ts
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? "https://akalp.co"),
   title: {
     default: "Hasan Akalp - Full-Stack Software Engineer",
     template: "%s | Hasan Akalp",
